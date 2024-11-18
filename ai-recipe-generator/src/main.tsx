fa953951354c76e6d5d6c51720513a9a1ba4a308
Gabriel Mathias <gablemathias@icloud.com>
2024-11-18 20:20:18 +0000

    connect to bedrock

--- ai-recipe-generator/src/main.tsx
+++ ai-recipe-generator/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { Authenticator } from "@aws-amplify/ui-react";
 
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Authenticator>
